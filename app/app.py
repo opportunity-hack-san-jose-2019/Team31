f6eb09ced49b57f48b4ae090266c137664e8eead
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-11-02 12:15:00 -0700

    Adding gitigore

--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
-flask import Flask
-
+from flask import Flask
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
